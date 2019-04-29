c52eca60052ad066f2bfa39cf4b0fa31212ed1b6
Lyserg.Z <lyserg.z@gmail.com>
2019-04-29 15:13:13 -0400

    First commit :O!

---
+++ chacha20-generator.js
@@ -0,0 +1,266 @@
+/**
+ * Interface to https://github.com/skeeto/chacha-js that will use a hex string as a seed for
+ * the key and IV. 32 byte key, 8 byte IV, 8 byte internal counter initialized to 0.
+ *
+ * The seed is assumed to be a 320 bits hex number represented by an 80 characters long string
+ * if it's longer, then it truncates it to its first 80 characters and discards the rest.
+ */
+
+// Quick function to get a hex string as an array of bytes
+// thanks https://stackoverflow.com/a/50868276/4192226
+const fromHexString = hexString => new Uint8Array(hexString.match(/.{1,2}/g).map(byte => parseInt(byte, 16)));
+const CHACHA_BLOCK = 64; //bytes
+
+var ChaChaRand = function(seed) {
+  // TODO validate the seed is actually a hex string
+  let keyHexSize = CHACHA_KEYSIZE*2;
+  let ivHexSize = CHACHA_IVSIZE*2;
+  let seedSize = keyHexSize+ivHexSize;
+  if(seed.length < seedSize) {
+    throw new Error("Seed should be a hex string at least seedSize characters long")
+  }
+  this._seed = seed.substr(0, seedSize);
+  this._key = fromHexString(seed.substr(0, keyHexSize));
+  this._iv = fromHexString(seed.substr(keyHexSize, ivHexSize));
+  // Actually, I guess I don't need to store any of ^that here, but I'll leave them for now just in case.
+  this._chacha = new ChaCha(this._key.buffer, this._iv.buffer);
+  // Get the first block right away (64 byte array buffer, turned into Uint8Array)
+  this._randBlock = new Uint8Array(this._chacha());
+  // Index of the next unused byte in the block
+  this._nextByteIndex = 0;
+  this._blockCounter = 1;
+
+  // // So you want individual bits...
+  // this._bitReserve = null;
+  // this._bitReserveCount = 0;
+};
+
+// TODO reseed method? If so, would you just throw away the current block, maybe decide based on a "throwBlock" param?
+//     (reseed would be practically the same as creating a new ChaChaRand with a new seed though, so it's whatever?)
+
+// How many blocks has this generator produced so far with its seed? (each block is 64 bytes)
+ChaChaRand.prototype.generatedBlocksCount = function() {
+  return this._blockCounter;
+};
+// How many bits have been requested. Somewhat ill defined in that the block may be generated as the chacha's state
+// well before any bytes are requested via getByte(s).
+ChaChaRand.prototype.usedBitsCount = function() {
+  return this._nextByteIndex*8 + ((this._blockCounter-1)*(CHACHA_BLOCK*8));
+};
+
+ChaChaRand.prototype._newRandBlock = function() {
+  // TODO handle the "output exhausted" error, and also... what should we do there?
+  this._randBlock = new Uint8Array(this._chacha());
+  this._blockCounter++;
+  this._nextByteIndex = 0;
+};
+
+// Get the amount of bytes specified
+ChaChaRand.prototype.getBytes = function(n) {
+  if(!n) {n = 0;}
+  if(n > 64 || n<=0){
+    // TODO just make all the blocks you need! it's like this just so it's simpler for now (and 64 is reasonable right?)
+    throw new Error("Number of bytes to return should be a minimum of 1 and maximum of 64");
+  }
+  let endIndex = this._nextByteIndex + n;
+  let ret = this._randBlock.slice(this._nextByteIndex, endIndex);
+  if(ret.length < n) {
+    // need a new block ... I just realised I can make this recursive, but that feels gross.
+    let missing = n - ret.length;
+    this._newRandBlock();
+    let old_ret = ret;
+    ret = new Uint8Array(n);
+    // Merging bytes from the old block with bytes from the new one
+    ret.set(old_ret);
+    ret.set(this._randBlock.slice(0, missing), old_ret.length);
+    this._nextByteIndex = missing;
+    return ret;
+  }
+  this._nextByteIndex += n;
+  return ret;
+};
+
+ChaChaRand.prototype.getByte = function() {
+  return this.getBytes(1);
+};
+
+// get a random positive number in [0 , 2**(nbits-1)), up to 53 bits
+ChaChaRand.prototype.getRandomBitsAsNum = function (nbits) {
+  if(nbits > 53 || nbits < 1) {throw new Error("The argument must be a positive number in [1,53]");}
+  let neededBytes = Math.ceil((nbits)/8);
+  let bytes = this.getBytes(neededBytes);
+  let ret = 0;
+  // bits that didn't consume an entire byte
+  let extraBits = nbits%8;
+  let bitmask = extraBits?((1<<extraBits)-1):-1;
+  // TODO recycle unused bits. I mean, asking for 1 single bit costs as much randomness as asking for a whole byte here!
+  ret = bytes[0] & bitmask;
+  for(let i = 1; i < neededBytes; i++) {
+    // should optimize by using bit shifts when nbits is small enough that working with int32 numbers is ok?
+    ret *= 256;
+    ret += bytes[i];
+  }
+  return ret;
+};
+
+// Get a ramdom number in [0, max]
+ChaChaRand.prototype.getRandomUInt = function(max) {
+  if(!max || max < 0) {
+    // I realise that if max===0 I can just return 0 right away instead, but if someone's asking for that,
+    // I feel like there's probably some error in their code somewhere. And with "someone" I mean me.
+    throw Error("Provide a positive max value for the number to be generated");
+  }
+  // TODO if max >= 2**53 should probably throw some error (which can be ignored via a second arg?)
+  // also, if we want to get signed ints, the range can be bigger than 2^53 numbers because negatives are available?
+  let maxbits = Math.floor(Math.log2(max))+1;
+  let ret = this.getRandomBitsAsNum(maxbits);
+  while(ret > max) {
+    ret = this.getRandomBitsAsNum(maxbits);
+  }
+  return ret;
+};
+
+// Rand in [min, max]
+ChaChaRand.prototype.getRandomIntInRange = function(min, max) {
+  return this.getRandomUInt(max - min) + min;
+};
+
+
+// For the following function:
+// Source https://github.com/davidbau/seedrandom/blob/released/seedrandom.js
+/*
+Copyright 2014 David Bau.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+// This function returns a random double in [0, 1) that contains
+// randomness in every bit of the mantissa of the IEEE 754 value.
+ChaChaRand.prototype.getRandomFloat = function() {
+  // David Bau's code and comments, but replacing RC4 with ChaCha20. Also replaced "math" with "Math" and var with let
+  // The following constants are related to IEEE 754 limits.
+  let width = 256,        // each ChaCha20 output is 0 <= x < 256
+      chunks = 6,         // at least six ChaCha20 outputs for each double
+      digits = 52,        // there are 52 significant digits in a double
+      startdenom = Math.pow(width, chunks),
+      significance = Math.pow(2, digits),
+      overflow = significance * 2;
+  let n = this.getRandomBitsAsNum(chunks*8),   // Start with a numerator n < 2 ^ 48
+      d = startdenom,                          //   and denominator d = 2 ^ 48.
+      x = 0;                                   //   and no 'extra last byte'.
+  while (n < significance) {                   // Fill up all significant digits by
+    n = (n + x) * width;                       //   shifting numerator and
+    d *= width;                                //   denominator and generating a
+    x = this.getRandomBitsAsNum(8);            //   new least-significant-byte.
+  }
+  while (n >= overflow) {                      // To avoid rounding up, before adding
+    n /= 2;                                    //   last byte, shift everything
+    d /= 2;                                    //   right using integer math until
+    x >>>= 1;                                  //   we have exactly the desired bits.
+  }
+  return (n + x) / d;                           // Form the number within [0, 1).
+};
+
+
+// Fisher Yates shuffle. Shuffles in place, so arr is modified.
+ChaChaRand.prototype.shuffle = function(arr) {
+  let n = arr.length;
+  for(let i = n-1; i > 0; i--){
+    let k = this.getRandomUInt(i);
+    // Exchanging arr_k with arr_i
+    let aux = arr[k];
+    arr[k] = arr[i];
+    arr[i] = aux;
+  }
+  // return arr; commented because it's in place and a return value might make you think it's not.
+};
+
+// Reservoir sampling (why private? because .sample is the real one. Note it returns indices, not arr's contents.
+ChaChaRand.prototype._reservoirSampling = function(arr, sampleSize) {
+  // TODO isn't just a partial Fisher Yates better? It loops less, spends the same amount of entropy... look into that
+  let n = arr.length;
+  if(sampleSize <= 0 || sampleSize>=n) {
+    // Could also throw if sampleSize = 1, because you can just use some vanilla rand number function in that case.
+    throw Error("Sample size must be a number between 1 and one less than the array size");
+  }
+  // this all's straight from wiki =P
+  let reservoir = [];
+  // fill the reservoir array
+  for(let i = 0; i < sampleSize; i++) {
+    reservoir.push(i);
+  }
+  // replace elements with gradually decreasing probability
+  for(let i = sampleSize; i < n; i++){
+    let rando = this.getRandomUInt(i);
+    if(rando < sampleSize) {
+      reservoir[rando] = i;
+    }
+  }
+  return reservoir;
+};
+
+// Random sample of arr. Returns randomly chosen indices to represent elements of the given array,
+// DOES NOT return the elements themselves.
+ChaChaRand.prototype.sample = function(arr, sampleSize) {
+  let n = arr.length;
+  if(sampleSize >= (n/2)) {
+    return this._reservoirSampling(arr, sampleSize);
+  }
+  else{
+    // In this case you can choose a sample of n - sampleSize and then return the ones you didn't choose...
+    // why do that? because the bigger the sample size the less iterations of the second _reservoirSampling loop,
+    // which means less entropy used.
+    let antiSize = n-sampleSize;
+    let antiSample = this._reservoirSampling(arr,antiSize);
+    let actualSample = [];
+    // Using a ".contains" based loop to get which arr elements aren't in antiSample feels inefficient, so let's try:
+    let auxSet = {};
+    for(let i = 0; i < antiSize; i++){
+      auxSet[antiSample[i]] = true;
+    }
+    for(let i = 0; i < n; i++) {
+      // === is me being overly paranoid of missing a bug because I checked for truthy values instead of "true"
+      if(auxSet[i]===true){continue;}
+      // If the index wasn't part of the anti sample, then it's part of the actual sample
+      actualSample.push(i);
+    }
+    return actualSample;
+  }
+  // Tempted to return a Set, to emphasize this isn't supposed to be a randomly ordered array, but just a random sample.
+  // (i.e. there's a bias towards the indices reflecting a 1...n order)
+};
+
+
+
+
+
+
+/**
+ let key  = new ArrayBuffer(CHACHA_KEYSIZE);
+ let iv   = new ArrayBuffer(CHACHA_IVSIZE);
+ let g    = new ChaCha(key, iv);
+ let v = new Uint8Array(g());
+ for (let i = 0; i < v.length; i++) {
+    pass = pass && v[i] == b0[i];
+ }
+ v = new Uint8Array(g());
+ for (let i = 0; i < v.length; i++) {
+    pass = pass && v[i] == b1[i];
+ }
+
+ */
